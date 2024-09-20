857cb58f69044e01d4f1ecf003b3730c0afbb16f
Nathan <79485616+FACETRU@users.noreply.github.com>
2024-09-20 11:46:51 -0300

    Add files via upload

---
+++ Leptospirose--p-coisa-p-/1-bloco-3-trimestre-main/Gráficos/quantidadeUsuarios.js
@@ -0,0 +1,33 @@
+import { getCSS } from "./common.js"
+
+async function quantidadeUsuariosPorRede() {
+    const url = 'file:///tmp/guest-i7gvev/%C3%81rea%20de%20Trabalho/lepo/lepo.html'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color')
+
+    }
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
+}
+
+quantidadeUsuariosPorRede()
